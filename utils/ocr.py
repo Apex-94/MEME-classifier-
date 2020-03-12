5bd95820c374583ee065d6a47488a67297560b9c
apex-94 <Viperrko94>
2020-03-12 09:34:09 +0530

    Patch 1 Reformat Code Minor changes

--- utils/ocr.py
+++ utils/ocr.py
@@ -25,17 +25,21 @@ from pathlib import Path
 import re
 import string
 import csv
+
 tesseract_cmd = 'tesseract'
+
 
 def give_temp_name():
     tmpfile = tempfile.NamedTemporaryFile(prefix="tess_")
     return tmpfile.name
+
 
 def del_file(filename):
     try:
         os.remove(filename)
     except OSError:
         pass
+
 
 def run_tesseract(input_filename, output_filename_base, lang=None):
     command = [tesseract_cmd, input_filename, output_filename_base]
@@ -49,26 +53,28 @@ def run_tesseract(input_filename, output_filename_base, lang=None):
     proc.stderr.close()
     return status, error_string
 
+
 def image_to_string(image):
-	if len(image.split()) == 4:
-		r,g,b,a = image.split()
-		image = Image.merge("RGB", (r,g,b))
+    if len(image.split()) == 4:
+        r, g, b, a = image.split()
+        image = Image.merge("RGB", (r, g, b))
 
-	input_file_name = '%s.bmp' % give_temp_name()
-	output_file_name_base = give_temp_name()
-	output_file_name = '%s.txt' % output_file_name_base
-	try:
-		image.save(input_file_name)
-		status, error_string = run_tesseract(input_file_name,output_file_name_base,lang = 'eng')
+    input_file_name = '%s.bmp' % give_temp_name()
+    output_file_name_base = give_temp_name()
+    output_file_name = '%s.txt' % output_file_name_base
+    try:
+        image.convert('RGB').save(input_file_name)
+        status, error_string = run_tesseract(input_file_name, output_file_name_base, lang='eng')
 
-		f = open(output_file_name, 'rb')
-		try:
-			return f.read().decode('utf-8').strip()
-		finally:
-			f.close()
-	finally:
-		del_file(input_file_name)
-		del_file(output_file_name)
+        f = open(output_file_name, 'rb')
+        try:
+            return f.read().decode('utf-8').strip()
+        finally:
+            f.close()
+    finally:
+        del_file(input_file_name)
+        del_file(output_file_name)
+
 
 # def spell_check(text):
 # 	text = text.replace(" u ", " you ").replace(' ur ', ' your ')
@@ -76,23 +82,25 @@ def image_to_string(image):
 # 	return text
 
 def main():
-    l=[]
-    i=0
+    l = []
+    i = 0
     # for filepath in glob.iglob(r"C:\Users\user\PycharmProjects\MEME-classifier-\data\offensive memes"):
     #     (l.append(filepath))
-    paths = Path(r'C:\Users\user\PycharmProjects\MEME-classifier-\data\offensive memes').glob('**/*.jpg')
+    paths = Path(r'C:\Users\user\PycharmProjects\MEME-classifier-\data\offensive memes').glob('**/*.png')
     for path in paths:
-    # for i in range(len(l)):
-        filename=str(path)
-	# filename = "in/" + input("Image of text to recognize: in/")
+        # for i in range(len(l)):
+        filename = str(path)
+        # filename = "in/" + input("Image of text to recognize: in/")
         image = Image.open(filename)
         text = image_to_string(image)
-        text = text.replace('\n',' ').lower()
-        text = re.sub(' +', ' ',text)
+        text = text.replace('\n', ' ').lower()
+        text = re.sub(' +', ' ', text)
         text = "".join(l for l in text if l not in string.punctuation)
         l.append(text)
         l.append(filename)
-
+        print("OCR sucessfull for ")
+        print(i)
+        i += 1
         # text = spell_check(text)
         # l.append(zip(text,filename))
         # # print ("Text recognized as: '" + str(text) + "'")
